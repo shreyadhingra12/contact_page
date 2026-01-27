4f1439d92a48612cce959919734794fb2cf95f5a
shreyadhingra12 <shreyadhingra31@gmail.com>
2026-01-27 18:38:49 +0530

    Added ContactHeader

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import ContactHeader from './component/ContactHeader/ContactHeader';
 import Navigation from './component/Navigation/Navigation';
 function App() {
   return (
     <div >
       <Navigation/>
+      <ContactHeader/>
     </div>
   );
 }
