5e4dd07cfb5bfcd9cff949ce68c5b6110deb7cd3
shreyadhingra12 <shreyadhingra31@gmail.com>
2026-01-29 14:06:14 +0530

    Added form

--- src/component/ContactForm/ContactForm.jsx
+++ src/component/ContactForm/ContactForm.jsx
@@ -1,17 +1,48 @@
-import styles from "./ContactForm.jsx"
+import Button from "../Buttons/Button.jsx";
+import styles from "./ContactForm.module.css";
+import { MdMessage } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
 
 const ContactForm = () => {
   return (
-    <section className={styles.Container}>
-        <div className={styles.Contact_form}>
+    <section className={styles.container}>
+      <div className={styles.contact_form}>
+        <div className={styles.top_btn}>
+          <Button
+            text="VIA SUPPORT CHAT"
+            icon={<MdMessage fontSize="24px" />}
+          />
+          <Button text="VIA CALL" icon={<FaPhoneAlt fontSize="24px" />} />
+        </div>
+        <Button
+          isOutline={true}
+          text="VIA EMAIL FORM"
+          icon={<IoIosMail fontSize="24px" />}
+        />
 
+        <form>
+          <div className={styles.form_control}>
+            <label htmlFor="name">Name</label>
+            <input type="text" input="Name" />
+          </div>
+          <div className={styles.form_control}>
+            <label htmlFor="email">E-Mail</label>
+            <input type="text" input="Email" />
+          </div>
+           <div className={styles.form_control}>
+            <label htmlFor="text">TEXT</label>
+            <textarea type="text" input="text" rows={20} />
+          </div>
+        </form>
+        <div style={{display:"flex",justifyContent:"end"}}>
+            <Button text="SUBMIT"/>
         </div>
-        <div className={styles.contact_image}>
-            <img src="/Users/shreya/Documents/project_react/contact_us/public/images/Service 24_7-pana 1 (1).svg" alt="24/7" />
-        </div>
+        
+      </div>
+      <div className={styles.contact_image}></div>
     </section>
-    
-  )
-}
+  );
+};
 
-export default ContactForm
+export default ContactForm;
